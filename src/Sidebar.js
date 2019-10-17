9b11e1ee3dce5e3d7fe0c492deba42d4ad2e7b27
amrit2k-droid <55663739+amrit2k-droid@users.noreply.github.com>
2019-10-17 18:36:24 +0530

    Update Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,12 +3,22 @@ import './StyleSidebar.css'
 
 class Sidebar extends React.Component{
 
+    constructor(props) {
+        super(props);
+        this.sendDataToParent = this.sendDataToParent.bind(this);
+    }
+    /*
+        This function transfers data from Sidebar to ShowSliderData
+    */
+    sendDataToParent = () => {
+        this.props.transferData(this.props.loanData, this.props.monthsData, this.props.interestData);
+    }
     render() {
 
             return (
                 <div className="contents">
                 <table>
-                    <tr>
+                    <tr onClick={this.sendDataToParent}>
                         <td>{this.props.stmt1}</td>
                         <td>{this.props.loanData}&nbsp;</td>
                         <td>{this.props.stmt2}&nbsp;</td>
